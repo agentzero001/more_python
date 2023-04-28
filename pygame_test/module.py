82bacb0ad4db020203e65b6fef9c67443702346a
agentzero001 <92370437+agentzero001@users.noreply.github.com>
2023-04-28 23:45:15 +0200

    event handling

--- pygame_test/module.py
+++ pygame_test/module.py
@@ -1,19 +1,30 @@
 import pygame as pg
 
+pg.init()
+screen = pg.display.set_mode((640, 480))
 
-def init():
-    pg.init()
-    win = pg.display.set_mode((400,400))
+# def getKey(kname):
+#    ans = True
+#    for eve in pg.event.get(): pass
+#    keyInput = pg.key.get_pressed()
+#    myKey = getattr(pg, 'K_{}'.format(kname))
+#
+#    return ans
 
-def getKey(kname):
-    ans = True
-    for event in pg.event.get(): pass
-    keyInput = pg.key.get_pressed()
-    myKey = getattr(pg, 'K_{}'.format(kname))
+x = 0
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                print('w pressed')
+            elif event.key == pg.K_k:
+                print('k pressed')
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_a]:
+        print('a pressed')
 
 
-
-if __name__ == '__main__':
-    init()
-
-
+# if __name__ == '__main__':
+#    init()
