7980f0067a98f856d710acb232a1fd9cffc45b52
agentzero001 <Jens.marhoefer001@gmail.com>
2023-05-17 20:46:05 +0200

    reveal yourself

--- cv/picture.py
+++ cv/picture.py
@@ -6,6 +6,5 @@ image = cv2.imread('captured_image.jpg')
 image_array = np.array(image)
 
 cv2.imshow('Image', image_array)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- cv/webcam.py
+++ cv/webcam.py
@@ -1,12 +1,18 @@
 import cv2
-
 a = 0
 cap = cv2.VideoCapture(0)
+a = 1
+
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:  
     ret, frame = cap.read()
     
-    cv2.imshow('Camera', frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    
+    cv2.imshow('Camera', gray_frame)
     
     if cv2.waitKey(1) & 0xFF == ord('p'):
         a+=1
