5a44361a483d058c76a918bfba356956543590f4
agentzero001 <Jens.marhoefer001@gmail.com>
2023-10-16 01:06:52 +0200

    main grid

--- pygame/main.py
+++ pygame/main.py
@@ -1,9 +1,36 @@
 import pygame as pg
 #print(pg.__version__)
 
+WIDTH, HEIGHT = 300, 300
+LINE_COLOR = (80, 80, 80)
+LINE_WIDTH = 5
+GRID_SIZE = WIDTH // 3
+X_COLOR = (255, 0, 0)
+O_COLOR = (0, 0, 255)
+
+
+board = [['' for _ in range(3)] for _ in range(3)]
+turn = 'X'
+game_over = False
+
+
 pg.init()
 
-screen = pg.display.set_mode((500, 500))
+screen = pg.display.set_mode((600, 600))
+pg.display.set_caption("Tic-Tac-Toe")
+
+def draw_board():
+    # pg.draw.line(screen, LINE_COLOR, (200,200), (200,400), LINE_WIDTH)
+    # pg.draw.line(screen, LINE_COLOR, (400,200), (400,400), LINE_WIDTH)
+    
+    # pg.draw.line(screen, LINE_COLOR, (200, 200), (400,200), LINE_WIDTH)
+    # pg.draw.line(screen, LINE_COLOR, (400,400), (200,400), LINE_WIDTH)
+
+    pg.draw.line(screen, LINE_COLOR, (200, 50), (200, 550), LINE_WIDTH)
+    pg.draw.line(screen, LINE_COLOR, (400, 50), (400, 550), LINE_WIDTH)
+     
+    pg.draw.line(screen, LINE_COLOR, (50, 200), (550, 200), LINE_WIDTH)
+    pg.draw.line(screen, LINE_COLOR, (50, 400), (550, 400), LINE_WIDTH)
 
 
 running = True
@@ -12,6 +39,7 @@ while running:
         if event.type == pg.QUIT:
             running = False
     
-    screen.fill((0, 0, 0))
+    screen.fill((20, 20, 20))
+    draw_board()
         
     pg.display.update()
