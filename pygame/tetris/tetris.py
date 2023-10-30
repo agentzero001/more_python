b747d1a2718fb3c77bfb39ba49b2958b9811312c
agentzero001 <Jens.marhoefer001@gmail.com>
2023-10-30 03:16:19 +0100

    -

--- pygame/tetris/tetris.py
+++ pygame/tetris/tetris.py
@@ -10,16 +10,16 @@ class Tetris:
         self.sprite_group = pg.sprite.Group()
         
     def draw_grid(self):
+        
         for x in range(FIELD_W):
-            for y in range(FIELD_H):
-                pg.draw.rect(self.app.screen, 'black',
-                            (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+           for y in range(FIELD_H):
+               pg.draw.rect(self.app.screen, 'black',
+                           (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
         
     def update(self):
-        #self.tetromino.update()
-        #self.sprite_group.update()
-        pass
-            
+        self.tetromino.update()
+        self.sprite_group.update()
+                    
     def draw(self):
         self.draw_grid()
         #self.sprite_group.draw(self.app.screen)
