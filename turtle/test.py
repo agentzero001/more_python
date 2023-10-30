ccb2e7dcf013b10b3fa564c14018a89c7c55ab98
agentzero001 <Jens.marhoefer001@gmail.com>
2023-10-30 22:21:02 +0100

    -

---
+++ turtle/test.py
@@ -0,0 +1,20 @@
+import turtle as t
+import colorsys
+
+
+t.speed(1000)
+
+t.bgcolor('black')
+h=0
+for i in range(16):
+    for j in range(18):
+        c = colorsys.hsv_to_rgb(h, 1, 1)
+        t.color(c)
+        h += 0.005
+        t.rt(90)
+        t.circle(150 -  j * 6, 90)
+        t.lt(90)
+        t.circle(150 -  j * 6, 90)
+        t.rt(180)
+    t.circle(40,24)
+t.done()    
