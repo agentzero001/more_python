6c14f00a4e864e3749491baf03828332b5704761
agentzero001 <Jens.marhoefer001@gmail.com>
2023-11-02 23:09:50 +0100

    -

--- pygame/tests_and_examples/main.py
+++ pygame/tests_and_examples/main.py
@@ -1,7 +1,39 @@
-import pygame.examples
-print(pygame.examples.__file__)
+import pygame as pg
+import sys, os
 
-import os 
 
-#print(os.path.split(os.path.abspath(__file__))[0])
-print(os.path.abspath(__file__))
+class Game:
+    def __init__(self, W_SIZE=600):
+        pg.init()
+        self.screen = pg.display.set_mode([W_SIZE] * 2)
+        self.clock = pg.time.Clock()
+        self.image = self.get_scaled_image(path='src/img1.jpg', res = (200,200))
+        
+        
+    @staticmethod
+    def get_scaled_image(path, res):
+        img = pg.image.load(path)
+        return pg.transform.smoothscale(img, res)
+        
+    def input(self, events):    
+        for event in events:
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            else:
+                print(event)
+                
+    def draw(self):
+        self.screen.blit(self.image, (100, 0))
+    
+    def run(self):
+        while True:
+            self.input(pg.event.get())
+            self.draw()
+            pg.display.update()
+    
+    
+    
+if __name__ == '__main__':
+    game = Game()
+    game.run()
--- pygame/tests_and_examples/pygame_testing.py
+++ pygame/tests_and_examples/pygame_testing.py
@@ -1,21 +1,34 @@
 import pygame as pg
 import module as m
-import sys
+import sys, os
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, "data")
 
 pg.init()
 
-pg.display.set_mode([200,200])
+pg.display.set_mode([500,500])
 
 running = m.running
 
-# def check_events():
-#     global running
-#     for event in pg.event.get():
-#         if event.type == pg.QUIT:
-#             pg.quit()
-#             sys.exit()
-#             running = False
+
       
+def load_image(name, colorkey=None, scale=1):
+    fullname = os.path.join(data_dir, name)
+    image = pg.image.load(fullname)
+    image = image.convert()
+
+    size = image.get_size()
+    size = (size[0] * scale, size[1] * scale)
+    image = pg.transform.scale(image, size)
+
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, pg.RLEACCEL)
+    return image, image.get_rect()     
+     
+     
       
 def input(events):
     global running
@@ -24,6 +37,8 @@ def input(events):
             running = False
         else:
             print(event)
+
+
 
 
 
