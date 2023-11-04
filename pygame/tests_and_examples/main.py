29bc1b94f37f8aba27a116b588a9c4da76e5f6ae
agentzero001 <Jens.marhoefer001@gmail.com>
2023-11-04 01:26:12 +0100

    -

--- pygame/tests_and_examples/main.py
+++ pygame/tests_and_examples/main.py
@@ -1,7 +1,6 @@
 import pygame as pg
 import sys, os
 
-
 class Game:
     def __init__(self, W_SIZE=600):
         pg.init()
@@ -9,12 +8,11 @@ class Game:
         self.clock = pg.time.Clock()
         self.image = self.get_scaled_image(path='src/img1.jpg', res = (200,200))
         
-        
     @staticmethod
     def get_scaled_image(path, res):
         img = pg.image.load(path)
         return pg.transform.smoothscale(img, res)
-        
+    
     def input(self, events):    
         for event in events:
             if event.type == pg.QUIT:
@@ -30,9 +28,7 @@ class Game:
         while True:
             self.input(pg.event.get())
             self.draw()
-            pg.display.update()
-    
-    
+            pg.display.update()    
     
 if __name__ == '__main__':
     game = Game()
