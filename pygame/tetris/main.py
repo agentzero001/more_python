b4d994da6b71606390edbb10d8a7ddba277d1272
agentzero001 <Jens.marhoefer001@gmail.com>
2023-11-05 21:25:51 +0100

    -

--- pygame/tetris/main.py
+++ pygame/tetris/main.py
@@ -10,14 +10,16 @@ class App:
         pg.display.set_caption('Tetris')
         self.screen = pg.display.set_mode(FIELD_RES)
         self.clock = pg.time.Clock()
-        self.set_timer()
+        #self.set_timer()
+        pg.time.set_timer(pg.USEREVENT, ANIM_TIME_INTERVAL)
         self.tetris = Tetris(self)
-        #self.new_game()
         
-    def set_timer(self):
-        self.user_event = pg.USEREVENT + 0
-        self.anim_trigger = False
-        pg.time.set_timer(self.user_event, ANIM_TIME_INTERVAL)
+    # def set_timer(self):
+    #     self.user_event = pg.USEREVENT + 0
+    #     self.anim_trigger = False
+    #     pg.time.set_timer(self.user_event, ANIM_TIME_INTERVAL)
+    
+    
         
     def new_game(self):
         pass
@@ -40,7 +42,7 @@ class App:
                 sys.exit()  
             elif event.type == pg.KEYDOWN:
                 self.tetris.control(event.key)
-            elif event.type == self.user_event:
+            elif event.type == pg.USEREVENT:
                 self.anim_trigger = True
 
     def run(self):
