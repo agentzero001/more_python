941be78b085f0d7e5a5bcb494c79a482bb512117
agentzero001 <Jens.marhoefer001@gmail.com>
2023-11-17 16:36:21 +0100

    galton board

--- pygame/galton_board/galton_board.py
+++ pygame/galton_board/galton_board.py
@@ -1,12 +1,10 @@
 import pymunk.pygame_util
 import pygame as pg
+from random import randrange
 pymunk.pygame_util.positive_y_is_up = False
 
 RES = WIDTH, HEIGHT = 1200, 900
 FPS = 60
-
-click = False
-a = 0
 
 pg.init()
 surface = pg.display.set_mode(RES)
@@ -14,9 +12,9 @@ clock = pg.time.Clock()
 draw_options = pymunk.pygame_util.DrawOptions(surface)
 
 space = pymunk.Space()
-space.gravity = 0, 2000
+space.gravity = 0, 6000
 
-ball_mass, ball_radius = 1, 7
+ball_mass, ball_radius = 1, 4
 segment_thickness = 6
 
 a, b, c, d = 10, 100, 18, 40
@@ -27,16 +25,15 @@ R1, R2, R3, R4 = (x4, -100), (x4, y1), (x3, y2), (x3, y3)
 B1, B2 = (0, HEIGHT), (WIDTH, HEIGHT)
 
 
-def create_ball(space, pos, mass, radius):
-    ball_mass = mass
-    ball_radius = radius
+def create_ball(space):
     ball_moment = pymunk.moment_for_circle(ball_mass, 0, ball_radius)
     ball_body = pymunk.Body(ball_mass, ball_moment)
-    ball_body.position = pos
+    ball_body.position = randrange(x1, x4), randrange(-y1, y1)
     ball_shape = pymunk.Circle(ball_body, ball_radius)
-    ball_shape.elasticity = .1
+    ball_shape.elasticity = .05
     ball_shape.friction = .1
     space.add(ball_body, ball_shape)
+    return ball_body
 
 def create_segment(begin, end, thickness, space, color): 
     segment_shape = pymunk.Segment(space.static_body, begin, end, thickness)
@@ -47,17 +44,27 @@ def create_segment(begin, end, thickness, space, color):
 def create_peg(x, y, space, color):
     circle_shape = pymunk.Circle(space.static_body, radius=10, offset=(x, y))
     circle_shape.color = pg.color.THECOLORS[color]
-    circle_shape.elasticity = 0.1
+    circle_shape.elasticity = 0.05
     circle_shape.friction = 0.5
     space.add(circle_shape)
     
-create_peg(200,300, space,'tomato')
+peg_y, step = y4, 60
+for i in range(10):
+    peg_x = -1.5 * step if i % 2 else -step
+    for j in range(WIDTH // step + 2):
+        create_peg(peg_x, peg_y, space, 'grey20')
+        if i == 9:
+            create_segment((peg_x, peg_y + 50), (peg_x, HEIGHT), segment_thickness, space, 'grey10')
+        peg_x += step
+    peg_y += .5 * step
     
 platforms = (L1, L2), (L2, L3), (L3, L4), (R1, R2), (R2, R3), (R3, R4)
 for platform in platforms:
     create_segment(*platform, segment_thickness, space, 'darkolivegreen')
 
 create_segment((0,HEIGHT), (WIDTH,HEIGHT), 20, space, 'darkolivegreen')
+
+balls = [(tuple(randrange(200) for i in range(3)), create_ball(space)) for j in range(2000)]
 
 while True:
     surface.fill(pg.Color('black'))
@@ -71,10 +78,15 @@ while True:
         if i.type == pg.MOUSEBUTTONUP:
                 create_ball(space, i.pos, ball_mass, ball_radius)
                 click = False
-                a = 0       
+                a = 0   
+                    
                     
     space.step(1 / FPS)
     space.debug_draw(draw_options)
+    
+    [pg.draw.circle(surface, color, (int(ball.position[0]), int(ball.position[1])),
+                    ball_radius)  for color, ball in balls]
+    
     pg.display.flip()
     clock.tick(FPS)
     
