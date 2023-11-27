93994260392f622a3bf998c194538f80fe4f083a
agentzero001 <Jens.marhoefer001@gmail.com>
2023-11-27 15:52:58 +0100

    curve

--- pygame/parametric_equations/main.py
+++ pygame/parametric_equations/main.py
@@ -1,38 +1,52 @@
 import sys 
+import numpy as np
 import pygame as pg
  
 pg.init()
 
-
 RES = WIDTH, HEIGHT = 800, 800
 SCALE_FACTOR = 1
 SIZE = 200
-K = 0
 FPS = 60
+GREY = (100,100,100)
+POINTS = [(-10, -10), (100, 100), (200, 200), (400, 200)]
 
+k = 0
 
+font = pg.font.Font(None, 24)
 fpsClock = pg.time.Clock()
 screen = pg.display.set_mode(RES)
 
-conv_coords = lambda x, y: ((x * SCALE_FACTOR + WIDTH // 2), (-y * SCALE_FACTOR + HEIGHT // 2))
+conv_coords = lambda x, y: ((x + WIDTH // 2), (-y  + HEIGHT // 2))
+inv_conv_coords = lambda x, y: conv_coords(-x, y)
+draw_point = lambda x, y, color=GREY, radius=2: pg.draw.circle(screen, color, conv_coords(x,y), radius)
 
-draw_point = lambda x, y, color=(100, 100, 100), radius=2: pg.draw.circle(screen, color, conv_coords(x,y), radius)
-
-#pg.draw.aalines
+a = lambda t: (100*np.sin(2*t), 100*np.cos(3*t))
 
 
-print(conv_coords(30, 30))
+def draw_curve(f, a, b, n=200):
+    t = np.linspace(a, b, n)
+    c_x, c_y = f(t)
+    pg.draw.aalines(screen, GREY, False, [conv_coords(x, y) for x,y in zip(c_x, c_y)]  , 1)      
+
+ 
 
 while True:
+    k %=1    
     screen.fill((20, 20, 20))
     pg.draw.line(screen, (100,100,100), conv_coords(-SIZE, 0), conv_coords(SIZE, 0), 1)
-    pg.draw.line(screen, (100,100,100), conv_coords(0, -SIZE), conv_coords(0, SIZE), 1)
-    draw_point(10,10)
+    pg.draw.line(screen, (100,100,100), conv_coords(0, -SIZE), conv_coords(0, SIZE), 1)    
+    draw_curve(a, 0, 2*np.pi*k)
+    k+=.001
+    m_x, m_y  = pg.mouse.get_pos()
+    m_x, m_y = m_x - WIDTH // 2, -(m_y - WIDTH // 2 )
+    text_surface = font.render(f"Mouse Position: ({m_x}, {m_y})", True, GREY)
+
   
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
-
+    screen.blit(text_surface, (10, 10))
     pg.display.flip()
     fpsClock.tick(FPS)
