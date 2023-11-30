2e4118ed692b07a2b7d3aa7070e253e53446e3d7
agentzero001 <Jens.marhoefer001@gmail.com>
2023-11-30 19:31:08 +0100

    -

--- pygame/Wolfenstein_3D/main.py
+++ pygame/Wolfenstein_3D/main.py
@@ -1,23 +1,24 @@
 import sys 
 import pygame as pg
 from settings import *
+import map_ as m
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
-        pg.time.set_timer(TIMER_EVENT, ANIM_TIME_INTERVAL)
+        #pg.time.set_timer(TIMER_EVENT, ANIM_TIME_INTERVAL)
+
+    def new_game(self):
+        pass
 
     def update(self):
         self.clock.tick(FPS) 
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
-         
-    def new_game(self):
-        pass
     
     def draw(self):
-        self.screen.fill('Black')
+        self.screen.fill('black')
         pg.display.flip() 
 
     def check_events(self):
