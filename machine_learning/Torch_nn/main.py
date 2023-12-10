7dee57b4ea4e8947e1420ec1167406fbeb6be86c
agentzero001 <Jens.marhoefer001@gmail.com>
2023-12-10 16:55:52 +0100

    Update main.py

--- machine_learning/Torch_nn/main.py
+++ machine_learning/Torch_nn/main.py
@@ -4,6 +4,8 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch import nn, optim, Tensor as t
 from torch.utils.data import DataLoader #easier dataset management (mini-batches, etc)
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -12,3 +14,6 @@ mnist_path = './data'
 train_dataset = datasets.MNIST(root=mnist_path, train=True, download=True)
 
 test_dataset = datasets.MNIST(root=mnist_path, train=False, download=True)
+
+img, label = train_dataset[0]
+
