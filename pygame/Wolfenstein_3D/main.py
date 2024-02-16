48ac1b2154e8d8726301f4df2d528fb1e854ace8
agentzero001 <Jens.marhoefer001@gmail.com>
2024-02-16 17:47:11 +0100

    -

--- pygame/Wolfenstein_3D/main.py
+++ pygame/Wolfenstein_3D/main.py
@@ -1,9 +1,9 @@
-import sys 
+import sys
 import pygame as pg
 from settings import *
 from player import *
 import map_
- 
+
 
 class Game:
     def __init__(self):
@@ -13,39 +13,59 @@ class Game:
         self.delta_time = 1
         self.new_game()
         self.count = 0
-        
+        self.toggle_overlay = False
+
+    def overlay(self):
+        text_color = (0, 0, 0)
+        overlay_surface = pg.Surface((RES), pg.SRCALPHA)
+        pg.draw.rect(overlay_surface, (100, 100, 100, 200), (10, 10, 200, 500))
+        font = pg.font.SysFont(None, 30)
+        score_text = font.render(f"map_pos: {self.player.map_pos}", True, text_color)
+        level_text = font.render(f"pos: {round(self.player.pos[0], 3)}, {round(self.player.pos[1], 3)}", True, text_color)
+        lives_text = font.render(f"dx, dy: {round(self.dx, 4)}, {round(self.dy, 4)}", True, text_color)
+        overlay_surface.blit(score_text, (15, 20))
+        overlay_surface.blit(level_text, (15, 40))
+        overlay_surface.blit(lives_text, (15, 60))
+        self.screen.blit(overlay_surface, (0, 0))
+
     def new_game(self):
         self.map1 = map_.Map(self)
         self.player = Player(self)
 
     def update(self):
-        self.player.update()
+        self.dx, self.dy = self.player.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
-        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
-        
+        pg.display.set_caption(f'{self.clock.get_fps():.1f}')
+
     def draw(self):
         self.screen.fill('black')
-        self.map1.draw()   
-        self.player.draw() 
- 
+        self.map1.draw()
+        self.player.draw()
+        if self.toggle_overlay:
+                self.overlay()
+        
+
     def check_events(self):
         self.anim_trigger = False
         for event in pg.event.get():
-            if (event.type == pg.QUIT 
-            or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE)):
+            if (event.type == pg.QUIT
+                    or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE)):
                 pg.quit()
                 sys.exit()
             elif event.type == TIMER_EVENT:
                 self.anim_trigger = True
-                
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_o:
+                    self.toggle_overlay = not self.toggle_overlay
+                                
+
     def run(self):
         while True:
             self.check_events()
             self.update()
             self.draw()
-            
-                      
+
 if __name__ == '__main__':
     app = Game()
     app.run()
--- pygame/Wolfenstein_3D/player.py
+++ pygame/Wolfenstein_3D/player.py
@@ -1,13 +1,14 @@
-from settings import  * 
+from settings import *
 import pygame as pg
 import math
+
 
 class Player:
     def __init__(self, game):
         self.game = game
         self.x, self.y = PLAYER_POS
         self.angle = PLAYER_ANGLE
-        
+
     def movement(self):
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
@@ -15,55 +16,55 @@ class Player:
         speed = PLAYER_SPEED * self.game.delta_time
         speed_sin = speed * sin_a
         speed_cos = speed * cos_a
-                
+
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
             dx += speed_cos
             dy += speed_sin
         if keys[pg.K_d]:
             dx += -speed_sin
-            dy += speed_cos        
+            dy += speed_cos
         if keys[pg.K_s]:
             dx += -speed_cos
-            dy += -speed_sin    
+            dy += -speed_sin
         if keys[pg.K_a]:
             dx += speed_sin
-            dy += -speed_cos 
-            
+            dy += -speed_cos
+
         self.check_wall_collision(dx, dy)
-        
+
         if keys[pg.K_q]:
             self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
         if keys[pg.K_e]:
             self.angle += PLAYER_ROT_SPEED * self.game.delta_time
-        self.angle %=  math.tau
+        self.angle %= math.tau
         
+        return dx, dy
+
     def check_wall(self, x, y):
         return (x, y) not in self.game.map1.world_map
-    
+
     def check_wall_collision(self, dx, dy):
         if self.check_wall(int(self.x + dx), int(self.y)):
             self.x += dx
         if self.check_wall(int(self.x), int(self.y + dx)):
             self.y += dy
-        
+
     def draw(self):
         pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
                      (self.x * 100 + WIDTH * math.cos(self.angle),
                       self.y * 100 + WIDTH * math.sin(self.angle)), 2)
-        pg.draw.circle(self.game.screen, (27, 79, 41), (self.x * 100, self.y * 100), 15)
-        
-        
+        pg.draw.circle(self.game.screen, (27, 79, 41),
+                       (self.x * 100, self.y * 100), 15)
+
     def update(self):
-        self.movement()
-        
-        
+        dx, dy = self.movement()
+        return dx, dy
+
     @property
     def pos(self):
         return self.x, self.y
-    
+
     @property
     def map_pos(self):
         return int(self.x), int(self.y)
-        
-        
