5f738fe10f7fe3698553a43486afcf24c80b5ce2
agentzero001 <Jens.marhoefer001@gmail.com>
2024-02-28 03:30:22 +0100

    just go step by step

--- pygame/breakout/main.py
+++ pygame/breakout/main.py
@@ -1,32 +1,40 @@
 import pygame as pg
 import sys, os
+import time
+from settings import *
 
 class Game:
-    def __init__(self, W_SIZE=600):
+    def __init__(self, W_SIZE=RES):
         pg.init()
-        self.screen = pg.display.set_mode((W_SIZE,) * 2)
+        self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
-        self.bg = self.get_scaled_image(path='src\img1.jpg', res = (W_SIZE,) * 2)
+        self.bg = self.get_scaled_image(path='src/graphics/other/bg.png', res=RES)
+        pg.display.set_caption('Breakout')
         
     @staticmethod
     def get_scaled_image(path, res):
         cwd = os.path.dirname(os.path.abspath(__file__))
-        img = pg.image.load(os.path.join(cwd, path))
-        return pg.transform.smoothscale(img, res)
+        img = pg.image.load(os.path.join(cwd, path)).convert()
+        scale_factor = HEIGHT / img.get_height()
+        scaled_width = img.get_width() * scale_factor
+        scaled_height = img.get_height() * scale_factor
+        return pg.transform.smoothscale(img, (scaled_width, scaled_height))
+        
     
     def input(self, events):    
         for event in events:
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            else:
-                print(event)
                 
     def draw(self):
         self.screen.blit(self.bg, (0, 0))
     
     def run(self):
+        last_time = time.time()
         while True:
+            dt = time.time() - last_time
+            last_time = time.time()
             self.input(pg.event.get())
             self.draw()
             pg.display.update()    
