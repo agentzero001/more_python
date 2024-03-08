5554a10ce8188d10dda9ce354c804c60367c2a3b
agentzero001 <Jens.marhoefer001@gmail.com>
2024-03-08 21:54:51 +0100

    -

--- machine_learning/Torch_nn/main.py
+++ machine_learning/Torch_nn/main.py
@@ -3,18 +3,80 @@ import torch.nn.functional as F
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch import nn, optim, Tensor as t
-from torch.utils.data import DataLoader #easier dataset management (mini-batches, etc)
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
+ 
 
+class NN(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 200)
+        self.fc2 = nn.Linear(200, num_classes)
+        
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
+    
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+input_size = 784
+num_classes = 10
+learning_rate = .001
+batch_size = 64
+num_epochs = 1
 
 mnist_path = './data'
+train_dataset = datasets.MNIST(root=mnist_path, train=True, download=True,
+                               transform=transforms.ToTensor())
 
-train_dataset = datasets.MNIST(root=mnist_path, train=True, download=True)
+test_dataset = datasets.MNIST(root=mnist_path, train=False, download=True, 
+                              transform=transforms.ToTensor())
 
-test_dataset = datasets.MNIST(root=mnist_path, train=False, download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader =  DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-img, label = train_dataset[0]
+
+model = NN(input_size, num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 
+for epoch in range(num_epochs):
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+        
+        data = data.reshape(data.shape[0], -1)
+        
+        scores = model(data)
+        loss = criterion(scores, targets)
+    
+        optimizer.zero_grad()
+        loss.backward()
+        
+        optimizer.step()
+        
+        
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+    
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+            x = x.reshape(x.shape[0], -1)
+            
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+            
+    return num_samples, num_correct
+            
+            
--- machine_learning/main.py
+++ machine_learning/main.py
@@ -7,12 +7,12 @@ from sklearn.utils.multiclass import unique_labels
 
 params = {
     'figure.facecolor': '.0',
-    'axes.facecolor': '.1',
-    'axes.labelcolor': 'white',
-    'text.color': 'white',
-    'xtick.color': 'green',
-    'ytick.color': 'green',
-    'axes.edgecolor': 'purple'
+    'axes.facecolor'  : '.1',
+    'axes.labelcolor' : 'white',
+    'text.color'      : 'white',
+    'xtick.color'     : 'green',
+    'ytick.color'     : 'green',
+    'axes.edgecolor'  : 'purple'
 }
 plt.rcParams.update(params)
 
@@ -22,24 +22,18 @@ class PerceptronEstimator(BaseEstimator, ClassifierMixin):
     def __init__(self, n_iterations=20, random_state=None):
         self.n_iterations = n_iterations
         self.random_state = random_state
-        self.errors = []
+        self.errors       = []
 
     
     heaviside = lambda self, x: 0 if x < 0 else 1    
 
     def fit(self, X=None, y=None ):
-        """ Trainieren       
-        X: Array-ähnliche Struktur mit [N,D], wobei 
-           N = Zeilen = Anzahl der Lernbeispiele und 
-           D = Spalten = Anzahl der Features 
-        y: Array mit [N], mit N so wie oben        
-        """ 
-        random_state = check_random_state(self.random_state)
-        self.w = random_state.random_sample(np.size(X,1))       
-        X, y = check_X_y(X, y)
+        random_state  = check_random_state(self.random_state)
+        self.w        = random_state.random_sample(np.size(X,1))
+        X, y          = check_X_y(X, y)
         self.classes_ = unique_labels(y)
-        self.X_ = X
-        self.y_ = y      
+        self.X_       = X
+        self.y_       = y
         for i in range(self.n_iterations):
             rand_index = random_state.randint(0,np.size(X,0))
             x_ = X[rand_index]
@@ -48,7 +42,6 @@ class PerceptronEstimator(BaseEstimator, ClassifierMixin):
             error = y_ - y_hat
             self.errors.append(error)
             self.w += error * x_
-            
         return self
     
     
@@ -59,7 +52,6 @@ class PerceptronEstimator(BaseEstimator, ClassifierMixin):
     
     
     def plot(self):                 
-    
         fignr = 1
         plt.figure(fignr,figsize=(5,5))#,facecolor='.2')   
         plt.plot(self.errors,color='green')
@@ -67,10 +59,8 @@ class PerceptronEstimator(BaseEstimator, ClassifierMixin):
         plt.xlabel('Iteration')
         plt.ylabel(r"$(y - \hat y)$")
         plt.show()
-
         
 def main():
-    
     X = np.array([[1,0,0]
                  ,[1,0,1],
                   [1,1,0],
