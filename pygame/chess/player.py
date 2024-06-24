68fa8499a28d52ea4187d85c8bee6a05dae67f08
agentzero001 <92370437+agentzero001@users.noreply.github.com>
2024-06-24 15:10:59 +0200

    HOTFIX 2

--- pygame/chess/player.py
+++ pygame/chess/player.py
@@ -7,14 +7,10 @@ class Player:
         self.app = app
         self.board = board
         self.pawns = self.draw_pawns(color)
-        self.pieces = []
         pieces = [Rook, Knight, Bishop, Queen, King, Bishop, Rook, Knight]
         y_pos = TILE_SIZE * 7 + TILE_SIZE_05 if color == 'white' else TILE_SIZE_05
-        for piece, x_pos in zip(pieces, range(8)):
-            p = piece(TILE_SIZE * x_pos + TILE_SIZE_05, y_pos, color, self.board)
-            self.pieces.append(p)
-            
-            
+        self.pieces = [piece(TILE_SIZE * x_pos + TILE_SIZE_05, y_pos, color, self.board) 
+                        for piece, x_pos in zip(pieces, range(8))]       
                    
     def draw_pawns(self, color):
         pawns = pg.sprite.Group()
@@ -27,6 +23,8 @@ class Player:
                 piece = Pawn(i , TILE_SIZE * 6 + TILE_SIZE // 2, color, self.board)
                 pawns.add(piece)
         return pawns
+    
+    
         
     def draw(self):
         self.pawns.draw(self.app.surface)
